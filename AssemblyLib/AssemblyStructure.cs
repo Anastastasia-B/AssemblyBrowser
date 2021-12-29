8b90e67280ee1a4f9e549ed7182604c5407efa56
Nastya Ber <anastasiaberz212@gmail.com>
2021-12-29 14:51:14 +0300

    create base  AssemblyLib structure

---
+++ AssemblyLib/AssemblyStructure.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using AssemblyLib.Node;
+
+namespace AssemblyLib
+{
+    public class AssemblyStructure
+    {
+        private AssemblyTree _assemblyTree;
+
+        public AssemblyStructure(string assemblyName)
+        {
+            Assembly assembly = Assembly.LoadFrom(assemblyName);
+            _assemblyTree = new AssemblyTree(assembly);
+        }
+
+        public void SetCustomNameFunc(TypeNode typeNode, Func<object, string> func)
+        {
+            _assemblyTree.SetCustomNameFunc(typeNode, func);
+        }
+
+        public List<AssemblyNode> GetAssemblyStructure()
+        {
+            return _assemblyTree.GetAssemblyTree();
+        }
+    }
+}
---
+++ AssemblyLib/AssemblyTree.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using AssemblyLib.Node;
+using AssemblyLib.Reflection;
+using AssemblyLib.Extension;
+
+namespace AssemblyLib
+{
+    internal class AssemblyTree
+    {
+        private Assembly _assembly;
+        private List<AssemblyNode> _tree;
+        private Dictionary<TypeNode, Func<object, string>> _nameFuncs;
+
+        internal AssemblyTree(Assembly assembly)
+        {
+            _assembly = assembly;
+            _tree = new List<AssemblyNode>();
+            _nameFuncs = new Dictionary<TypeNode, Func<object, string>>()
+            {
+                { TypeNode.Namespace, NameFormatter.GetNamespaceName },
+                { TypeNode.Type, NameFormatter.GetTypeName },
+                { TypeNode.Field, NameFormatter.GetFieldName },
+                { TypeNode.Property, NameFormatter.GetPropertyName },
+                { TypeNode.Method, NameFormatter.GetMethodSignature }
+            };
+        }
+
+        internal void SetCustomNameFunc(TypeNode typeNode, Func<object, string> func)
+        {
+            if (_nameFuncs.ContainsKey(typeNode))
+            {
+                _nameFuncs[typeNode] = func;
+            }
+            else
+            {
+                throw new Exception("Not exist for this type generator");
+            }
+        }
+
+        private void CreateTree()
+        {
+        }
+
+
+        internal List<AssemblyNode> GetAssemblyTree()
+        {
+            CreateTree();
+            return _tree.OrderBy(node => node.Name).ToList();
+        }
+    }
+}
