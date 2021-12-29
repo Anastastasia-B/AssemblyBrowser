213e34f5f54fcb5381b36f975fefe0c129f1002d
Nastya Ber <anastasiaberz212@gmail.com>
2021-12-29 14:54:06 +0300

    get real metadata

--- AssemblyLib/AssemblyTree.cs
+++ AssemblyLib/AssemblyTree.cs
@@ -43,8 +43,43 @@ namespace AssemblyLib
 
         private void CreateTree()
         {
+            AssemblyNode currentNode = null;
+            Type[] types = Metadata.GetAssemblyTypes(_assembly);
+            for (int i = 0; i < types.Length; i++)
+            {
+                currentNode = _tree.GetOrAdd(new AssemblyNode(TypeNode.Namespace, types[i], _nameFuncs[TypeNode.Namespace](types[i])));
+                currentNode = currentNode.Childrens.GetOrAdd(new AssemblyNode(TypeNode.Type, types[i], _nameFuncs[TypeNode.Type](types[i])));
+                currentNode.Childrens.AddRange(GetMembers(types[i]));
+            }
         }
 
+        private List<AssemblyNode> GetMembers(Type type)
+        {
+            List<AssemblyNode> result = new List<AssemblyNode>();
+            MethodInfo[] methods = Metadata.GetMethods(type);
+            if (type.IsDefined(typeof(ExtensionAttribute)))
+            {
+                methods = DeleteExtensionMethods(methods);
+            }
+            result.AddRange(methods.Select(method => new AssemblyNode(TypeNode.Method, method, _nameFuncs[TypeNode.Method](method))).ToList());
+            result.AddRange(Metadata.GetFields(type).Select(field => new AssemblyNode(TypeNode.Field, field, _nameFuncs[TypeNode.Field](field))).ToList());
+            result.AddRange(Metadata.GetProperties(type).Select(property => new AssemblyNode(TypeNode.Property, property, _nameFuncs[TypeNode.Property](property))).ToList());
+            return result;
+        }
+
+        private MethodInfo[] DeleteExtensionMethods(MethodInfo[] methods)
+        {
+            AssemblyNode currentNode = null;
+            MethodInfo[] extensionMethods = methods.Where(method => method.IsDefined(typeof(ExtensionAttribute))).ToArray();
+            for (int i = 0; i < extensionMethods.Length; i++)
+            {
+                Type extensionType = extensionMethods[i].GetParameters()[0].ParameterType;
+                currentNode = _tree.GetOrAdd(new AssemblyNode(TypeNode.Namespace, extensionType, _nameFuncs[TypeNode.Namespace](extensionType)));
+                currentNode = currentNode.Childrens.GetOrAdd(new AssemblyNode(TypeNode.Type, extensionType, _nameFuncs[TypeNode.Type](extensionType)));
+                currentNode.Childrens.Add(new AssemblyNode(TypeNode.Method, extensionMethods[i], _nameFuncs[TypeNode.Method](extensionMethods[i])));
+            }
+            return methods.Except(extensionMethods).ToArray();
+        }
 
         internal List<AssemblyNode> GetAssemblyTree()
         {
---
+++ AssemblyLib/Reflection/Metadata.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace AssemblyLib.Reflection
+{
+    internal static class Metadata
+    {
+        private const BindingFlags _flags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.DeclaredOnly;
+
+        internal static Type[] GetAssemblyTypes(Assembly assembly)
+        {
+            Type[] assemblyTypes = new Type[0];
+            try
+            {
+                assemblyTypes = assembly.GetTypes();
+            }
+            catch (ReflectionTypeLoadException ex)
+            {
+                assemblyTypes = ex.Types.Where(type => type != null).ToArray();
+            }
+            return assemblyTypes;
+        }
+
+        internal static MethodInfo[] GetMethods(Type type)
+        {
+            return type.GetMethods(_flags).Where(method => !method.IsSpecialName).ToArray();
+        }
+
+        internal static FieldInfo[] GetFields(Type type)
+        {
+            return type.GetFields(_flags).Where(field => !field.IsDefined(typeof(CompilerGeneratedAttribute))).ToArray();
+        }
+
+        internal static PropertyInfo[] GetProperties(Type type)
+        {
+            return type.GetProperties(_flags).Where(property => !property.IsSpecialName).ToArray();
+        }
+    }
+}
