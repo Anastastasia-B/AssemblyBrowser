3f2b7a4a80544832135a20cf899c6c09a6e68e9a
Nastya Ber <anastasiaberz212@gmail.com>
2021-12-29 14:37:53 +0300

    implement INotifyPropertyChanged

--- AssemblyBrowser/Model/Assembly.cs
+++ AssemblyBrowser/Model/Assembly.cs
@@ -7,5 +7,32 @@ using AssemblyLib.Node;
 
 namespace AssemblyBrowser.Model
 {
+    public class Assembly : INotifyPropertyChanged
+    {
+        private AssemblyStructure _assemblyStructure;
+        public List<AssemblyNode> Nodes
+        {
+            get
+            {
+                return _assemblyStructure?.GetAssemblyStructure();
+            }
+        }
 
+        public Assembly(string assemblyName)
+        {
+            _assemblyStructure = new AssemblyStructure(assemblyName);
+        }
+
+        public void SetCustomNameFunc(TypeNode typeNode, Func<object, string> func)
+        {
+            _assemblyStructure.SetCustomNameFunc(typeNode, func);
+            OnPropertyChanged("Nodes");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
 }
--- AssemblyBrowser/ViewModel/MainViewModel.cs
+++ AssemblyBrowser/ViewModel/MainViewModel.cs
@@ -1,9 +1,31 @@
 ﻿using Microsoft.Win32;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using AssemblyBrowser.Command;
 using AssemblyBrowser.Model;
 
 namespace AssemblyBrowser.ViewModel
 {
-   
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private Assembly _assembly;
+        public Assembly Assembly
+        {
+            get
+            {
+                return _assembly;
+            }
+            set
+            {
+                _assembly = value;
+                OnPropertyChanged("Assembly");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
 }
